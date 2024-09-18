2f74d055518f506858ab912bbcc8d0709a28bafa
laukik86 <laukikpadgaonkar0@gmail.com>
2024-09-18 12:30:37 +0530

    cleared frontend

--- driveto/src/App.js
+++ driveto/src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-import Navbar from "./Components/Navbar";
-import Options from "./Components/Options";
-import Home from "./Screens/Home";
+
 
 function App() {
   return (
     <>
-    <Home></Home>
-    <Navbar></Navbar>
-    <Options></Options>
+    <h1>heloo</h1>
     </>
   );
 }
